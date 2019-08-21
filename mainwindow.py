06883e60cbaf978759bbc64c33c088fa40a0c7b5
igrekus <upload234@mail.ru>
2019-08-21 17:09:12 +0300

    WIP: test draw arcs

--- mainwindow.py
+++ mainwindow.py
@@ -1,17 +1,19 @@
 import datetime
 import os
-from math import sqrt
+import numpy
+import euclid3
+
+from numpy import arccos, cos, sin, sqrt, arcsin, arctan2
 
 from PyQt5 import uic
 from PyQt5.QtCore import Qt, pyqtSlot
 from PyQt5.QtWidgets import QMainWindow, QFileDialog, QGraphicsView, QGraphicsRectItem, QGraphicsScene, \
     QGraphicsLineItem, QGraphicsEllipseItem
+
 from pygcode import Line, GCodeMotion, GCodeLinearMove, GCodeArcMoveCW, GCodeArcMoveCCW
 
-import euclid3
 
-
-zoom = 10
+zoom = 20
 
 
 class LineSegmentItem(QGraphicsLineItem):
@@ -31,11 +33,19 @@ class LineSegmentItem(QGraphicsLineItem):
 
 
 class ArcItem(QGraphicsEllipseItem):
-    def __init__(self, x0, y0, x1, y1, i, j):
+    def __init__(self, x0, y0, x1, y1, i, j, cw):
         super().__init__()
+        self.x0 = x0
+        self.y0 = y0
+        self.x1 = x1
+        self.y1 = y1
+        self.i = i
+        self.j = j
+        self.cw = cw
         self._center = euclid3.Point2(i, j)
-        self._radius = sqrt(pow(x1 - i, 2) + pow(y1 - j, 2)) * zoom
-        self._circle = euclid3.Circle(self._center, self._radius)
+        self._radius = float(sqrt(pow(x1 - i, 2) + pow(y1 - j, 2))) * zoom
+
+        self._circle = euclid3.Circle(euclid3.Point2(i, j), self._radius)
 
         self._num_segments = 10
         self._segments = list()
@@ -69,7 +79,8 @@ class MainWindow(QMainWindow):
         print(self._coil_length())
 
     def parse_cnc(self):
-        filename = './gcode/output_0002.ngc'
+        # filename = './gcode/output_0002.ngc'
+        filename = 'gcode/VTESLIN.CNC'
         with open(filename, 'rt') as f:
             for text_line in f.readlines():
                 text_line.replace(r'\\', '#')
@@ -88,13 +99,15 @@ class MainWindow(QMainWindow):
             params = path.block.gcodes[0].get_param_dict()
             if 'X' not in params or 'Y' not in params:
                 continue
-            x1, y1 = params['X'], -params['Y']
+            x1, y1 = params['X'], params['Y']
             if isinstance(path.block.gcodes[0], GCodeLinearMove):
                 self._geometry.append(LineSegmentItem(x0, y0, x1, y1))
             elif isinstance(path.block.gcodes[0], GCodeArcMoveCW) or isinstance(path.block.gcodes[0], GCodeArcMoveCCW):
                 if 'I' not in params or 'J' not in params:
                     continue
-                self._geometry.append(ArcItem(x0, y0, x1, y1, params['I'], -params['J']))
+                cw = isinstance(path.block.gcodes[0], GCodeArcMoveCW)
+                i, j = params['I'], params['J']
+                self._geometry.append(ArcItem(x0, y0, x1, y1, i, j, cw))
             x0, y0 = x1, y1
 
     def _draw(self):
@@ -103,8 +116,43 @@ class MainWindow(QMainWindow):
         # https://buildmedia.readthedocs.org/media/pdf/euclid/latest/euclid.pdf   -   geometry lib
         # https://stackoverflow.com/questions/11331854/how-can-i-generate-an-arc-in-numpy
         # http://pycam.sourceforge.net/
+
+        def inv_parametric_circle(x, xc, r):
+            t = arccos((x - xc) / r)
+            return t
+
+        def parametric_circle(ts, xc, yc, R):
+            for t in ts:
+                x, y = xc + R * cos(t), yc + R * sin(t)
+                yield x, y
+
+        steps = 10
+
         for line in self._geometry:
-                self.scene.addItem(line)
+            self.scene.addItem(line)
+            if isinstance(line, ArcItem):
+                if line.cw:
+                    arc = line
+
+                    # 1 quadrant
+                    angle_start = arctan2((arc.y0 - arc.j), (arc.x0 - arc.i))
+                    angle_end = arctan2((arc.y1 - arc.j), (arc.x1 - arc.i))
+                    print(angle_start, angle_end)
+
+                    # ---------
+                    r = arc._radius
+                    center_x = arc._center.x * zoom
+                    center_y = arc._center.y * zoom
+
+                    start_point = (center_x + r * cos(angle_start), center_y + r * sin(angle_start))
+                    end_point = (center_x + r * cos(angle_end), center_y + r * sin(angle_end))
+
+                    start_t = inv_parametric_circle(start_point[0], center_x, r)
+                    end_t = inv_parametric_circle(end_point[0], center_x, r)
+                    arc_T = numpy.linspace(start_t, end_t, steps)
+
+                    for x, y in parametric_circle(arc_T, center_x, center_y, r):
+                        self.scene.addRect(x, y, 2, 2)
 
     def _coil_length(self):
         return sum([line.length for line in self._geometry])
