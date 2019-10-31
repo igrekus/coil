f6e4a7265dada6672d3df4571c6491c6145be467
igrekus <upload234@mail.ru>
2019-10-31 14:23:51 +0300

    TEST: finalize test for empty cnc-file, add test file

---
+++ tests/header_footer_only.cnc
@@ -0,0 +1,8 @@
+//CNC-Profile written by CN-Coil Designer
+//Datetime: 10.25.2019 - 16:16:00
+%
+:
+G71
+G90
+
+M30
--- tests/test_cnfile.py
+++ tests/test_cnfile.py
@@ -1,10 +1,11 @@
-import os
 from pyexpect import expect
 from cncoilgcode import CNFile
 
-file = '../gcode/VCOMTEST.CNC'
 
+def test_parse_empty_cnfile():
+    cnc = CNFile('header_footer_only.cnc')
 
-def test_parse_cnfile():
-    cnc = CNFile(file)
+    expect(len(cnc._header)).to_equal(6)
+    expect(len(cnc._commands)).to_equal(0)
+    expect(len(cnc._footer)).to_equal(2)
 
