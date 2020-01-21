8ac01b9354ea2e86776d15e862f9ff760d82e831
igrekus <upload234@mail.ru>
2020-01-21 17:20:56 +0300

    WIP: add tests for weld command

--- tests/test_static_commands.py
+++ tests/test_static_commands.py
@@ -1,6 +1,7 @@
 from pyexpect import expect
 
 from cncode import FillCommand
+from cncode.static_commands import WeldCommand
 
 
 def test_fillcommand_constructor():
@@ -21,3 +22,23 @@ def test_fillcommand_from_string():
     expect(fill.is_move).to_equal(False)
     expect(fill.as_gcode).to_equal('N001 M501 P1.0 P0.002\nG04 P0.002')
     expect([fill[i] for i in range(10)]).to_equal([1, 'Fill', '', '', '', '', '', 1.0, 2.0, 0.0])
+
+
+def test_weldcommand_constructor():
+    weld = WeldCommand(1, 1.1)
+
+    expect(weld.length).to_equal(0)
+    expect(weld.disabled).to_equal((2, 3, 4, 5, 6, 7, 8))
+    expect(weld.is_move).to_equal(False)
+    expect(weld.as_gcode).to_equal('N001 M70 P1 P0\n')
+    expect([weld[i] for i in range(10)]).to_equal([1, 'Weld', '', '', '', '', '', '', '', 1.1])
+
+
+def test_weldcommand_from_string():
+    weld = WeldCommand.from_string('N003 M70 P20 P0\n')
+
+    expect(weld.length).to_equal(0)
+    expect(weld.disabled).to_equal((2, 3, 4, 5, 6, 7, 8))
+    expect(weld.is_move).to_equal(False)
+    expect(weld.as_gcode).to_equal('N003 M70 P20 P0\n')
+    expect([weld[i] for i in range(10)]).to_equal([3, 'Weld', '', '', '', '', '', '', '', 20.0])
