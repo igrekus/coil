e057ab9cf14f6379df835940da124bb5e5a09ea5
igrekus <upload234@mail.ru>
2020-01-21 15:38:49 +0300

    REF: move enum definitions back into the base class file

--- cncode/bases.py
+++ cncode/bases.py
@@ -1,6 +1,51 @@
 from abc import ABC, abstractmethod
+from enum import Enum
 
-from cncode import CommandType
+
+class CommandType(Enum):
+    UNDEFINED, \
+        FILL, \
+        WELD, \
+        SONO_UP, \
+        SONO_MID, \
+        SONO_LOW, \
+        CUT_WIRE, \
+        EMBED_ON, \
+        EMBED_OFF, \
+        PULL_WIRE, \
+        HOLD_MODULE, \
+        RELEASE_MODULE, \
+        BRAKE_ON, \
+        BRAKE_OFF, \
+        THERM_MID, \
+        THERM_UP, \
+        LINE_TO, \
+        CW_ARC_TO, \
+        CCW_ARC_TO, \
+        LINE_TO_END, \
+        LINE_TO_START, \
+        LINE_TO_BOTH = range(22)
+
+
+move_commands = [
+    CommandType.LINE_TO,
+    CommandType.CW_ARC_TO,
+    CommandType.CCW_ARC_TO,
+    CommandType.LINE_TO_END,
+    CommandType.LINE_TO_START,
+    CommandType.LINE_TO_BOTH
+]
+
+
+class ArcType(Enum):
+    SHORT, LONG = range(2)
+
+
+class ArcDirection(Enum):
+    CW, CCW = range(2)
+
+
+arc_label = {ArcType.SHORT: 'Short', ArcType.LONG: 'Long'}
 
 
 class Command(ABC):
