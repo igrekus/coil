4835185b5a99fbea0fe804c65de5b0f7c287fec3
igrekus <upload234@mail.ru>
2020-01-21 15:16:43 +0300

    WIP: implement Fill cn-coil command

--- cncode/static_commands.py
+++ cncode/static_commands.py
@@ -1,43 +1,39 @@
+from pygcode import Line
+
 from cncode.bases import Command, CommandType
 
 
 class FillCommand(Command):
-    def __init__(self, text, previous=None):
-        super().__init__(text, previous)
-        self._label = 'Fill'
-        self._type = CommandType.FILL
-
-        self._parse()
+    def __init__(self, index: int=0, spill: float=0.0, delay: float=0.0):
+        super().__init__(type_=CommandType.FILL,
+                         index=index,
+                         label='Fill',
+                         spill=spill,
+                         delay=delay,
+                         prm=0.0)
 
     def __str__(self):
         return f'FillCnCommand(n={self._index} p1={self._spill} p2={self._delay})'
 
-    def __getitem__(self, item):
-        if item in range(2, 7):
-            return ''
-        elif item == 0:
-            return self._index
-        elif item == 1:
-            return self._label
-        elif item == 7:
-            return self._spill
-        elif item == 8:
-            return self._delay
-        elif item == 9:
-            return ''
+    @property
+    def disabled(self):
+        return 2, 3, 4, 5, 6, 9
 
-    def _parse(self):
-        super()._parse()
-        assert len(self._cnc_lines) == 2
+    @property
+    def as_gcode(self):
+        return f'N{self._index:.03d} M501 P{self._spill} P.{str(self._delay).replace(".", "")}\n' \
+               f'G04 P.{str(self._delay).replace(".", "")}'
 
-        line1, line2 = self._cnc_lines
+    @classmethod
+    def from_string(cls, string: str):
+        cnc_lines = [Line(l) for l in string.strip().split('\n')]
+        assert len(cnc_lines) == 2
+
+        line1, line2 = cnc_lines
         assert line1.gcodes[0].word_letter == 'N'
         assert line2.gcodes[0].word == 'G04'
 
-        self._index = line1.gcodes[0].number
-        self._spill = line1.block.modal_params[1].value
-        self._delay = line1.block.modal_params[2].value * 1000
-
-    @property
-    def enabled(self):
-        return 1, 7, 8
+        index = line1.gcodes[0].number
+        spill = line1.block.modal_params[1].value
+        delay = line1.block.modal_params[2].value * 1000
+        return cls(index=index, spill=spill, delay=delay)
