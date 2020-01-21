25547e5fa7bb59e79c668ca11a952f8fa1dc1e79
igrekus <upload234@mail.ru>
2020-01-21 17:41:03 +0300

    REF: rename temp command variable to simplify test

--- tests/test_static_commands.py
+++ tests/test_static_commands.py
@@ -5,66 +5,66 @@ from cncode.bases import CommandType
 
 
 def test_fillcommand_constructor():
-    fill = FillCommand(1, 1, 1)
+    com = FillCommand(1, 1, 1)
 
-    expect(fill.command_type).to_equal(CommandType.FILL)
-    expect(fill.length).to_equal(0)
-    expect(fill.disabled).to_equal((2, 3, 4, 5, 6, 9))
-    expect(fill.is_move).to_equal(False)
-    expect(fill.as_gcode).to_equal('N001 M501 P1 P0.001\nG04 P0.001\n')
-    expect([fill[i] for i in range(10)]).to_equal([1, 'Fill', '', '', '', '', '', 1.0, 1.0, 0.0])
+    expect(com.command_type).to_equal(CommandType.FILL)
+    expect(com.length).to_equal(0)
+    expect(com.disabled).to_equal((2, 3, 4, 5, 6, 9))
+    expect(com.is_move).to_equal(False)
+    expect(com.as_gcode).to_equal('N001 M501 P1 P0.001\nG04 P0.001\n')
+    expect([com[i] for i in range(10)]).to_equal([1, 'Fill', '', '', '', '', '', 1.0, 1.0, 0.0])
 
 
 def test_fillcommand_from_string():
-    fill = FillCommand.from_string('N001 M501 P1 P.002\nG04 P.002\n')
+    com = FillCommand.from_string('N001 M501 P1 P.002\nG04 P.002\n')
 
-    expect(fill.command_type).to_equal(CommandType.FILL)
-    expect(fill.length).to_equal(0)
-    expect(fill.disabled).to_equal((2, 3, 4, 5, 6, 9))
-    expect(fill.is_move).to_equal(False)
-    expect(fill.as_gcode).to_equal('N001 M501 P1.0 P0.002\nG04 P0.002\n')
-    expect([fill[i] for i in range(10)]).to_equal([1, 'Fill', '', '', '', '', '', 1.0, 2.0, 0.0])
+    expect(com.command_type).to_equal(CommandType.FILL)
+    expect(com.length).to_equal(0)
+    expect(com.disabled).to_equal((2, 3, 4, 5, 6, 9))
+    expect(com.is_move).to_equal(False)
+    expect(com.as_gcode).to_equal('N001 M501 P1.0 P0.002\nG04 P0.002\n')
+    expect([com[i] for i in range(10)]).to_equal([1, 'Fill', '', '', '', '', '', 1.0, 2.0, 0.0])
 
 
 def test_weldcommand_constructor():
-    weld = WeldCommand(1, 1.1)
+    com = WeldCommand(1, 1.1)
 
-    expect(weld.command_type).to_equal(CommandType.WELD)
-    expect(weld.length).to_equal(0)
-    expect(weld.disabled).to_equal((2, 3, 4, 5, 6, 7, 8))
-    expect(weld.is_move).to_equal(False)
-    expect(weld.as_gcode).to_equal('N001 M70 P1 P0\n')
-    expect([weld[i] for i in range(10)]).to_equal([1, 'Weld', '', '', '', '', '', '', '', 1.1])
+    expect(com.command_type).to_equal(CommandType.WELD)
+    expect(com.length).to_equal(0)
+    expect(com.disabled).to_equal((2, 3, 4, 5, 6, 7, 8))
+    expect(com.is_move).to_equal(False)
+    expect(com.as_gcode).to_equal('N001 M70 P1 P0\n')
+    expect([com[i] for i in range(10)]).to_equal([1, 'Weld', '', '', '', '', '', '', '', 1.1])
 
 
 def test_weldcommand_from_string():
-    weld = WeldCommand.from_string('N003 M70 P20 P0\n')
+    com = WeldCommand.from_string('N003 M70 P20 P0\n')
 
-    expect(weld.command_type).to_equal(CommandType.WELD)
-    expect(weld.length).to_equal(0)
-    expect(weld.disabled).to_equal((2, 3, 4, 5, 6, 7, 8))
-    expect(weld.is_move).to_equal(False)
-    expect(weld.as_gcode).to_equal('N003 M70 P20 P0\n')
-    expect([weld[i] for i in range(10)]).to_equal([3, 'Weld', '', '', '', '', '', '', '', 20.0])
+    expect(com.command_type).to_equal(CommandType.WELD)
+    expect(com.length).to_equal(0)
+    expect(com.disabled).to_equal((2, 3, 4, 5, 6, 7, 8))
+    expect(com.is_move).to_equal(False)
+    expect(com.as_gcode).to_equal('N003 M70 P20 P0\n')
+    expect([com[i] for i in range(10)]).to_equal([3, 'Weld', '', '', '', '', '', '', '', 20.0])
 
 
 def test_sonoupcommand_constructor():
-    sonoup = SonoUpCommand(1, 1.1)
+    com = SonoUpCommand(1, 1.1)
 
-    expect(sonoup.command_type).to_equal(CommandType.SONO_UP)
-    expect(sonoup.length).to_equal(0)
-    expect(sonoup.disabled).to_equal((2, 3, 4, 5, 6, 7, 8))
-    expect(sonoup.is_move).to_equal(False)
-    expect(sonoup.as_gcode).to_equal('N001 M71 P1 P0\n')
-    expect([sonoup[i] for i in range(10)]).to_equal([1, 'Sono Up', '', '', '', '', '', '', '', 1.1])
+    expect(com.command_type).to_equal(CommandType.SONO_UP)
+    expect(com.length).to_equal(0)
+    expect(com.disabled).to_equal((2, 3, 4, 5, 6, 7, 8))
+    expect(com.is_move).to_equal(False)
+    expect(com.as_gcode).to_equal('N001 M71 P1 P0\n')
+    expect([com[i] for i in range(10)]).to_equal([1, 'Sono Up', '', '', '', '', '', '', '', 1.1])
 
 
 def test_sonoupcommand_from_string():
-    sonoup = SonoUpCommand.from_string('N004 M71 P20 P0\n')
+    com = SonoUpCommand.from_string('N004 M71 P20 P0\n')
 
-    expect(sonoup.command_type).to_equal(CommandType.SONO_UP)
-    expect(sonoup.length).to_equal(0)
-    expect(sonoup.disabled).to_equal((2, 3, 4, 5, 6, 7, 8))
-    expect(sonoup.is_move).to_equal(False)
-    expect(sonoup.as_gcode).to_equal('N004 M71 P20 P0\n')
-    expect([sonoup[i] for i in range(10)]).to_equal([4, 'Sono Up', '', '', '', '', '', '', '', 20.0])
+    expect(com.command_type).to_equal(CommandType.SONO_UP)
+    expect(com.length).to_equal(0)
+    expect(com.disabled).to_equal((2, 3, 4, 5, 6, 7, 8))
+    expect(com.is_move).to_equal(False)
+    expect(com.as_gcode).to_equal('N004 M71 P20 P0\n')
+    expect([com[i] for i in range(10)]).to_equal([4, 'Sono Up', '', '', '', '', '', '', '', 20.0])
