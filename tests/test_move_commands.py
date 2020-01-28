10589453a5d8543f94baaa359b62642450a908da
igrekus <upload234@mail.ru>
2020-01-28 14:44:29 +0300

    WIP: add tests for line to cn command

--- tests/test_move_commands.py
+++ tests/test_move_commands.py
@@ -1,3 +1,4 @@
+from euclid3 import LineSegment2, Point2
 from pyexpect import expect
 
 from cncode import LineToCommand
@@ -5,24 +6,47 @@ from cncode.bases import CommandType
 
 
 def test_linetocommand_constructor():
-    com = LineToCommand(1, 10, 10, 12000, 0, 0, 0, 0, 0)
+    com = LineToCommand(1, 10.0, 10.0, 12000, 0, None, None)
 
     expect(com.command_type).to_equal(CommandType.LINE_TO)
+    expect([com[i] for i in range(10)]).to_equal([1, 'Line To', 10, 10, '*', '', 12000, 0, '', ''])
+    expect(com.is_move).to_equal(True)
+    expect(com.length).almost_equal(14.14, 0.01)
+    expect(com.disabled).to_equal((5, 8, 9))
 
-    print(com)
-    # expect(com.length).to_equal(0)
-    # expect(com.disabled).to_equal((2, 3, 4, 5, 6, 9))
-    # expect(com.is_move).to_equal(False)
-    # expect(com.as_gcode).to_equal('N001 M501 P1 P0.001\nG04 P0.001\n')
-    # expect([com[i] for i in range(10)]).to_equal([1, 'Fill', '', '', '', '', '', 1.0, 1.0, 0.0])
+    gui = com.gui_geometry[-1]
+    expect(gui.p1).to_equal(Point2(0, 0))
+    expect(gui.p2).to_equal(Point2(10, 10))
+
+    gcode = com.gcode_geometry[-1]
+    expect(gcode.p1).to_equal(Point2(0, 0))
+    expect(gcode.p2).to_equal(Point2(10, 10))
+
+    expect(com.gcode_end_x).to_equal(10.0)
+    expect(com.gcode_end_y).to_equal(10.0)
+
+    expect(com.as_gcode).to_equal('N001 M500 P0\n     F12000\n     G01 X10.0 Y10.0 Z0\n')
 
 
-# def test_linetocommand_from_string():
-#     com = LineToCommand.from_string('N001 M501 P1 P.002\nG04 P.002\n')
-#
-#     expect(com.command_type).to_equal(CommandType.FILL)
-#     expect(com.length).to_equal(0)
-#     expect(com.disabled).to_equal((2, 3, 4, 5, 6, 9))
-#     expect(com.is_move).to_equal(False)
-#     expect(com.as_gcode).to_equal('N001 M501 P1.0 P0.002\nG04 P0.002\n')
-#     expect([com[i] for i in range(10)]).to_equal([1, 'Fill', '', '', '', '', '', 1.0, 2.0, 0.0])
+def test_linetocommand_from_string():
+    com = LineToCommand.from_string(string='N001 M500 P1\n     F12000\n     G01 X0 Y5 Z0\n',
+                                    prev_gui_end=Point2(0, 0), prev_gcode_end=Point2(0, 0))
+
+    expect(com.command_type).to_equal(CommandType.LINE_TO)
+    expect([com[i] for i in range(10)]).to_equal([1, 'Line To', 0, 5.0, '*', '', 12000, 1.0, '', ''])
+    expect(com.is_move).to_equal(True)
+    expect(com.length).to_equal(5)
+    expect(com.disabled).to_equal((5, 8, 9))
+
+    gui = com.gui_geometry[-1]
+    expect(gui.p1).to_equal(Point2(0, 0))
+    expect(gui.p2).to_equal(Point2(0, 5))
+
+    gcode = com.gcode_geometry[-1]
+    expect(gcode.p1).to_equal(Point2(0, 0))
+    expect(gcode.p2).to_equal(Point2(0, 5))
+
+    expect(com.gcode_end_x).to_equal(0)
+    expect(com.gcode_end_y).to_equal(5)
+
+    expect(com.as_gcode).to_equal('N001 M500 P1.0\n     F12000\n     G01 X0.0 Y5.0 Z0\n')
