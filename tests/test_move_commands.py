9a1f4edeaaecbcef281081acb95ecfc31cbfafeb
igrekus <upload234@mail.ru>
2020-02-04 19:24:48 +0300

    WIP: add tests for cw short arc to command

--- tests/test_move_commands.py
+++ tests/test_move_commands.py
@@ -2,7 +2,8 @@ from euclid3 import LineSegment2, Point2
 from pyexpect import expect
 
 from cncode import LineToCommand, LineToWithEndCurveCommand, CwShortArcToCommand
-from cncode.bases import CommandType
+from cncode.bases import CommandType, ArcType
+from cncode.move_commands import Arc
 
 
 def test_linetocommand_constructor():
@@ -99,23 +100,26 @@ def test_linetocommand_from_string():
 #     expect(com.as_gcode).to_equal('N001 M500 P1.0\n     F12000\n     G01 X0.0 Y5.0 Z0\n')
 
 def test_cwarcshortcommand_constructor():
-    com = CwShortArcToCommand(1, 5, 0, 3, 12000, 0, None, None)
+    com = CwShortArcToCommand(1, 5.0, 3.0, 4.0, 12000, 0, Point2(0, 0), Point2(0, 0))
 
-    # expect(com.command_type).to_equal(CommandType.LINE_TO)
-    # expect([com[i] for i in range(10)]).to_equal([1, 'Line To', 10.0, 10.0, 2.0, '', 12000, 0, '', ''])
-    # expect(com.is_move).to_equal(True)
+    expect(com.command_type).to_equal(CommandType.CW_ARC_TO_SHORT)
+    expect([com[i] for i in range(10)]).to_equal([1, 'CW Arc To', 5.0, 3.0, 4.0, 0, 12000, 0, '', ''])
+    expect(com.is_move).to_equal(True)
     # expect(com.length).almost_equal(14.14, 0.01)
-    # expect(com.disabled).to_equal((5, 8, 9))
+    expect(com.disabled).to_equal((8, 9))
 
-    # gui = com.gui_geometry[-1]
-    # expect(gui.p1).to_equal(Point2(0, 0))
-    # expect(gui.p2).to_equal(Point2(10, 10))
-    #
-    gcode = com.gcode_geometry[-1]
-    # expect(gcode.p1).to_equal(Point2(0, 0))
-    # expect(gcode.p2).to_equal(Point2(10, 10))
-    #
-    # expect(com.gcode_end_x).to_equal(10.0)
-    # expect(com.gcode_end_y).to_equal(10.0)
-    #
+    gui = com.gui_geometry[-1]
+    expect(len(com.gui_geometry)).to_equal(1)
+    expect(gui.p1).to_equal(Point2(0, 0))
+    expect(gui.p2).to_equal(Point2(5.0, 3.0))
+    expect(type(gui)).to_be(Arc)
+    expect(gui.c.x).almost_equal(3.909, 0.01)
+    expect(gui.c.y).almost_equal(-0.84, 0.01)
+    expect(gui.r).almost_equal(4, 0.01)
+
+    expect(com.length).almost_equal(6.53, 0.01)
+
+    expect(com.gcode_end_x).to_equal(5.0)
+    expect(com.gcode_end_y).to_equal(3.0)
+
     # expect(com.as_gcode).to_equal('N001 M500 P0\n     F12000\n     G01 X10.0 Y10.0 Z0\n')
