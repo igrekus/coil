b3246901dc8faa99fe8d42fe6a10ef6a4dbea015
igrekus <upload234@mail.ru>
2020-02-05 13:40:01 +0300

    TEST: add tests for cw short arc to command

--- tests/test_move_commands.py
+++ tests/test_move_commands.py
@@ -122,3 +122,29 @@ def test_cwarcshortcommand_constructor():
     expect(com.gcode_end_y).to_equal(3.0)
 
     expect(com.as_gcode).to_equal('N001 M500 P0\n     F12000\n     G02 X5.000 Y3.000 Z0 I3.909 J-0.848 K0\n')
+
+
+def test_cwarcshortcommand_from_string():
+    com = CwShortArcToCommand.from_string(string='N001 M500 P0\n     F12000 \n     G02 X-2 Y2 Z0 I.8708286934 J2.8708286934 K0',
+                                          prev_gui_end=Point2(0, 0), prev_gcode_end=Point2(0, 0))
+
+    expect(com.command_type).to_equal(CommandType.CW_ARC_TO_SHORT)
+    expect([com[i] for i in range(10)]).to_equal([1, 'CW Arc To', -2.0, 2.0, 3.0, 0, 12000, 0, '', ''])
+    expect(com.is_move).to_equal(True)
+    expect(com.disabled).to_equal((8, 9))
+
+    gui = com.gui_geometry[-1]
+    expect(len(com.gui_geometry)).to_equal(1)
+    expect(gui.p1).to_equal(Point2(0, 0))
+    expect(gui.p2).to_equal(Point2(-2.0, 2.0))
+    expect(type(gui)).to_be(Arc)
+    expect(gui.c.x).almost_equal(0.871, 0.01)
+    expect(gui.c.y).almost_equal(2.871, 0.01)
+    expect(gui.r).almost_equal(3, 0.01)
+
+    expect(com.length).almost_equal(2.95, 0.01)
+
+    expect(com.gcode_end_x).to_equal(-2.0)
+    expect(com.gcode_end_y).to_equal(2.0)
+
+    expect(com.as_gcode).to_equal('N001 M500 P0.0\n     F12000\n     G02 X-2.000 Y2.000 Z0 I0.871 J2.871 K0\n')
